0e8f07d6de0f96a0966afda266d70e74b06f3250
Alex <Alwong@ya.ru>
2023-02-26 22:44:58 +0400

    first commit

---
+++ app/Http/Controllers/Product/EditController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Product;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use App\Models\Tag;
+use App\Models\Color;
+use App\Models\Category;
+
+class EditController extends Controller
+{
+    public function __invoke(Product $product)
+    {
+
+        $tags = Tag::all();
+        $colors = Color::all();
+        $categories = Category::all();
+
+        $productTagIds = [];
+        $productColorIds = [];
+        foreach($product->tags as $productTag) {
+            array_push($productTagIds, $productTag->id);
+        }
+        foreach($product->colors as $productColor) {
+            array_push($productColorIds, $productColor->id);
+        }
+
+        return view('product.edit', compact('product', 'tags', 'colors', 'categories', 'productTagIds', 'productColorIds'));
+    }
+}
---
+++ app/Models/Product.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $table = 'products';
+    protected $guarded = false;
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }  
+    
+    public function colors()
+    {
+        return $this->belongsToMany(Color::class);
+    }        
+}
