55faa33a4341bc1836f0b33a2996c040ec357847
Alex <Alwong@ya.ru>
2023-03-05 22:58:33 +0400

    add pagination and filter

--- app/Http/Controllers/API/Product/IndexController.php
+++ app/Http/Controllers/API/Product/IndexController.php
@@ -3,15 +3,21 @@
 namespace App\Http\Controllers\API\Product;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\API\Product\IndexRequest;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Http\Resources\Product\IndexProductResource;
+use App\Http\Filters\ProductFilter;
 
 class IndexController extends Controller
 {
-    public function __invoke()
+    public function __invoke(IndexRequest $request)
     {
-        $products = Product::all();
+
+        $data = $request->validated();
+        $filter = app()->make(ProductFilter::class, ['queryParams' => array_filter($data)]);
+
+        $products = Product::filter($filter)->paginate(12, ['*'], 'page', $data['page']);
 
         return IndexProductResource::collection($products);
     }
